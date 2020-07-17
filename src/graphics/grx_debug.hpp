666a5bcad260706f391ee9c6cf036cf874e4ae3b
ptrNine <sockskilling@gmail.com>
2020-07-17 22:54:16 +0300

    Remove loading shaders from files in grx_debug
    Also reformat code with clang-format

--- src/graphics/grx_debug.hpp
+++ src/graphics/grx_debug.hpp
@@ -2,113 +2,113 @@
 
 #include "grx_vbo_tuple.hpp"
 
-#include <core/helper_macros.hpp>
 #include <core/config_manager.hpp>
-#include <glm/mat4x4.hpp>
+#include <core/helper_macros.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/mat4x4.hpp>
 
-#include "grx_types.hpp"
 #include "grx_context.hpp"
-#include "grx_shader_mgr.hpp"
+#include "grx_shader.hpp"
+#include "grx_types.hpp"
 
-namespace grx {
+namespace grx
+{
 
-    class grx_aabb_debug_drawer {
-        SINGLETON_IMPL(grx_aabb_debug_drawer);
+/**
+ * @brief Singleton for drawing AABB's and collision shapes
+ *
+ * Use grx_aabb_debug to access the instance of this class
+ *
+ */
+class grx_aabb_debug_drawer {
+    SINGLETON_IMPL(grx_aabb_debug_drawer);
 
-    private:
-        ~grx_aabb_debug_drawer() = default;
-        grx_aabb_debug_drawer() {
-            _box.set_data<0>({
-                { 1.f, -1.f, -1.f},
-                {-1.f, -1.f, -1.f},
-                {-1.f,  1.f, -1.f},
-                { 1.f, -1.f, -1.f},
-                {-1.f,  1.f, -1.f},
-                { 1.f,  1.f, -1.f},
+private:
+    ~grx_aabb_debug_drawer() = default;
+    grx_aabb_debug_drawer() {
+        /* 2x2x2 rectangle */
+        _box.set_data<0>({{1.f, -1.f, -1.f},  {-1.f, -1.f, -1.f}, {-1.f, 1.f, -1.f},
+                          {1.f, -1.f, -1.f},  {-1.f, 1.f, -1.f},  {1.f, 1.f, -1.f},
 
-                { 1.f, -1.f,  1.f},
-                { 1.f, -1.f, -1.f},
-                { 1.f,  1.f, -1.f},
-                { 1.f, -1.f,  1.f},
-                { 1.f,  1.f, -1.f},
-                { 1.f,  1.f,  1.f},
+                          {1.f, -1.f, 1.f},   {1.f, -1.f, -1.f},  {1.f, 1.f, -1.f},
+                          {1.f, -1.f, 1.f},   {1.f, 1.f, -1.f},   {1.f, 1.f, 1.f},
 
-                {-1.f, -1.f,  1.f},
-                { 1.f, -1.f,  1.f},
-                { 1.f,  1.f,  1.f},
-                {-1.f, -1.f,  1.f},
-                { 1.f,  1.f,  1.f},
-                {-1.f,  1.f,  1.f},
+                          {-1.f, -1.f, 1.f},  {1.f, -1.f, 1.f},   {1.f, 1.f, 1.f},
+                          {-1.f, -1.f, 1.f},  {1.f, 1.f, 1.f},    {-1.f, 1.f, 1.f},
 
-                {-1.f, -1.f, -1.f},
-                {-1.f, -1.f,  1.f},
-                {-1.f,  1.f,  1.f},
-                {-1.f, -1.f, -1.f},
-                {-1.f,  1.f,  1.f},
-                {-1.f,  1.f, -1.f},
+                          {-1.f, -1.f, -1.f}, {-1.f, -1.f, 1.f},  {-1.f, 1.f, 1.f},
+                          {-1.f, -1.f, -1.f}, {-1.f, 1.f, 1.f},   {-1.f, 1.f, -1.f},
 
-                { 1.f,  1.f, -1.f},
-                {-1.f,  1.f, -1.f},
-                {-1.f,  1.f,  1.f},
-                { 1.f,  1.f, -1.f},
-                {-1.f,  1.f,  1.f},
-                { 1.f,  1.f,  1.f},
+                          {1.f, 1.f, -1.f},   {-1.f, 1.f, -1.f},  {-1.f, 1.f, 1.f},
+                          {1.f, 1.f, -1.f},   {-1.f, 1.f, 1.f},   {1.f, 1.f, 1.f},
 
-                { 1.f, -1.f,  1.f},
-                {-1.f, -1.f,  1.f},
-                {-1.f, -1.f, -1.f},
-                { 1.f, -1.f,  1.f},
-                {-1.f, -1.f, -1.f},
-                { 1.f, -1.f, -1.f}
-            });
+                          {1.f, -1.f, 1.f},   {-1.f, -1.f, 1.f},  {-1.f, -1.f, -1.f},
+                          {1.f, -1.f, 1.f},   {-1.f, -1.f, -1.f}, {1.f, -1.f, -1.f}});
 
-            using core::operator/;
+        auto shader_vs = grx_shader<shader_type::vertex>(
+            "uniform mat4 MVP;",
+            "in vec3 position_ms;"
+            "void main() { gl_Position = MVP * vec4(position_ms, 1.0); }");
 
-            auto shader_dir = core::DEFAULT_CFG_PATH() / "../" /
-                core::config_section::direct_read().read_unwrap<core::string>("shaders_dir");
-            auto section = core::config_section::direct_read("shader_aabb_debug", core::cfg_reentry("debug_tools"));
+        auto shader_fs = grx_shader<shader_type::fragment>(
+            "uniform vec3 aabb_color;",
+            "out vec4 color;",
+            "void main() { color = vec4(aabb_color, 1.0); }");
 
-            _program_id = _sm.compile_program(
-                    shader_dir / section.read_unwrap<core::string>("effect_path"),
-                    section.read_unwrap<core::string>("entry_function"));
-            _mvp_uniform_id   = _sm.get_uniform_id_unwrap(_program_id, "MVP");
-            _color_uniform_id = _sm.get_uniform_id_unwrap(_program_id, "aabb_color");
-        }
+        _program = grx_shader_program::create_shared(shader_vs, shader_fs);
 
-    public:
-        void draw(const grx_aabb& aabb, const glm::mat4& model, const glm::mat4& view_projection, const core::vec3f& color) const {
-            auto scale = (aabb.max - aabb.min) / core::vec{2.f, 2.f, 2.f};
-            auto pos   = (aabb.max + aabb.min) / core::vec{2.f, 2.f, 2.f};
-
-            auto transform = glm::scale(glm::translate(glm::mat4(1.f), core::to_glm(pos)), core::to_glm(scale));
-
-            //core::printline("pos: {.3}  scale: {.3}", pos, scale);
-
-            _box.bind_vao();
-
-            _sm.use_program(_program_id);
-            _sm.set_uniform(_mvp_uniform_id, view_projection * (model * transform));
-            _sm.set_uniform(_color_uniform_id, color);
-
-            //grx_ctx().set_depth_test_enabled(false);
-            grx_ctx().set_cull_face_enabled(false);
-            grx_ctx().set_wireframe_enabled(true);
-            _box.draw(36);
-            grx_ctx().set_wireframe_enabled(false);
-            grx_ctx().set_cull_face_enabled(true);
-            //grx_ctx().set_depth_test_enabled(true);
-        }
-
-    private:
-        grx_vbo_tuple<vbo_vector_vec3f> _box;
-        grx_shader_mgr                  _sm;
-        shader_program_id_t             _program_id;
-        uniform_id_t                    _mvp_uniform_id;
-        uniform_id_t                    _color_uniform_id;
-    };
-
-    inline grx_aabb_debug_drawer& grx_aabb_debug() {
-        return grx_aabb_debug_drawer::instance();
+        _mvp   = _program->get_uniform_unwrap<glm::mat4>("MVP");
+        _color = _program->get_uniform_unwrap<core::vec3f>("aabb_color");
     }
+
+public:
+    /**
+     * @brief Draws aabb
+     *
+     * @param aabb - the aabb
+     * @param model - the model matrix
+     * @param view_projection - (projection * view) matrix from camera
+     * @param color - the color
+     */
+    void draw(const grx_aabb&    aabb,
+              const glm::mat4&   model,
+              const glm::mat4&   view_projection,
+              const core::vec3f& color) const {
+        auto scale = (aabb.max - aabb.min) / core::vec{2.f, 2.f, 2.f}; // NOLINT
+        auto pos   = (aabb.max + aabb.min) / core::vec{2.f, 2.f, 2.f}; // NOLINT
+
+        auto transform = glm::scale(glm::translate(glm::mat4(1.f), core::to_glm(pos)), core::to_glm(scale));
+
+        _box.bind_vao();
+
+        _program->activate();
+        _mvp   = view_projection * (model * transform);
+        _color = color;
+
+        grx_ctx().set_cull_face_enabled(false);
+        grx_ctx().set_wireframe_enabled(true);
+
+        constexpr size_t rectangle_vertices_count = 36;
+        _box.draw(rectangle_vertices_count);
+
+        grx_ctx().set_wireframe_enabled(false);
+        grx_ctx().set_cull_face_enabled(true);
+    }
+
+private:
+    grx_vbo_tuple<vbo_vector_vec3f>      _box;
+    core::shared_ptr<grx_shader_program> _program;
+    grx_uniform<glm::mat4>               _mvp;
+    grx_uniform<core::vec3f>             _color;
+};
+
+/**
+ * @brief Gets reference to grx_aabb_debug_drawer singleton instance
+ *
+ * @return the reference to grx_aabb_debug_drawer singleton instance
+ */
+inline grx_aabb_debug_drawer& grx_aabb_debug() {
+    return grx_aabb_debug_drawer::instance();
 }
+} // namespace grx
+
