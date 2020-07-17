b4576c380ff53f5eea0aaf3b9a685e77b3048494
ptrNine <sockskilling@gmail.com>
2020-07-17 23:05:46 +0300

    Remove unused code from grx_shader_mgr.hpp

--- src/graphics/grx_shader_mgr.hpp
+++ src/graphics/grx_shader_mgr.hpp
@@ -1,131 +1,56 @@
 #pragma once
 
+#include "grx_shader.hpp"
 #include "grx_types.hpp"
+#include <core/md5.hpp>
 #include <core/vec.hpp>
 
-namespace core {
-    class config_manager;
-    class config_section;
+namespace core
+{
+class config_manager;
 }
 
-namespace std {
-    template <>
-    struct hash<pair<string, grx::shader_program_id_t>> {
-        inline size_t operator()(const pair <string, grx::shader_program_id_t>& p) const {
-            return std::hash<string>()(p.first) ^ std::hash<grx::shader_program_id_t>()(p.second);
-        }
-    };
-
-    template <>
-    struct hash<pair<grx::shader_effect_id_t, string>> {
-        inline size_t operator()(const pair<grx::shader_effect_id_t, string>& p) const {
-            return std::hash<grx::shader_effect_id_t>()(p.first) ^ std::hash<string>()(p.second);
-        }
-    };
+namespace grx
+{
+namespace shader_helper
+{
+    template <shader_type T>
+    using strview_eval_t = core::string_view;
 }
 
-namespace grx {
-    class grx_shader_tech {
-    private:
-        friend class grx_shader_mgr;
-        grx_shader_tech(shader_program_id_t base, shader_program_id_t skeleton, shader_program_id_t instanced):
-            _base(base), _skeleton(skeleton), _instanced(instanced) {}
+class grx_shader_program_mgr : public std::enable_shared_from_this<grx_shader_program_mgr> {
+public:
+    template <shader_type... Types>
+    core::shared_ptr<grx_shader_program> load(shader_helper::strview_eval_t<Types>... paths) {
+        static_assert(sizeof...(Types) > 0);
 
-        shader_program_id_t _base;
-        shader_program_id_t _skeleton;
-        shader_program_id_t _instanced;
+        core::string key              = ((core::path_eval(paths) + ";") + ...);
+        auto [position, was_inserted] = _program_map.emplace(key, nullptr);
 
-    public:
-        DECLARE_VAL_GET(base)
-        DECLARE_VAL_GET(skeleton)
-        DECLARE_VAL_GET(instanced)
-    };
+        if (was_inserted)
+            position->second = grx_shader_program::create_shared(load_shader<Types>(core::path_eval(paths))...);
 
+        return position->second;
+    }
 
-    class grx_shader_compiler {
-    public:
-        shader_program_id_t compile(const core::string& effect);
-    };
+    core::shared_ptr<grx_shader_program> load(const core::config_manager& config_manager, core::string_view section) {
+        /*
+         * Now we use shader section name as key.
+         * That not compatible with constructor from paths
+         * i.e shader program constructed from identical shaders paths wouldn't be replaced.
+         *
+         * I think it's normal way, because shader program read from config may contain uniforms and
+         * therefore can't replace shader program (or can't be replaced by the shader program) from plain files
+         */
+        auto [position, was_inserted] = _program_map.emplace(core::string(section), nullptr);
 
+        if (was_inserted)
+            position->second = grx_shader_program::create_shared(config_manager, section);
 
-    class grx_shader_mgr {
-    public:
-        ~grx_shader_mgr();
+        return position->second;
+    }
 
-        shader_program_id_t compile_program(
-                const core::string& effect_path,
-                const core::string& function_name);
-
-        shader_program_id_t compile_program(
-                const core::config_manager& cm,
-                core::string_view shader_section_name);
-
-        grx_shader_tech load_render_tech(
-                const core::config_manager& cm,
-                core::string_view section);
-
-        shader_program_id_t compile_program(
-                const core::config_section& cm,
-                core::string_view shader_section_name);
-
-        grx_shader_tech load_render_tech(
-                const core::config_section& cm,
-                core::string_view section);
-
-
-        core::optional<uniform_id_t>
-        get_uniform_id(shader_program_id_t program, const core::string& name);
-
-        uniform_id_t
-        get_uniform_id_unwrap(shader_program_id_t program, const core::string& name);
-
-        static void set_uniform(uniform_id_t id, int val1);
-        static void set_uniform(uniform_id_t id, int val1, int val2);
-        static void set_uniform(uniform_id_t id, int val1, int val2, int val3);
-        static void set_uniform(uniform_id_t id, int val1, int val2, int val3, int val4);
-
-        static void set_uniform(uniform_id_t id, uint val1);
-        static void set_uniform(uniform_id_t id, uint val1, uint val2);
-        static void set_uniform(uniform_id_t id, uint val1, uint val2, uint val3);
-        static void set_uniform(uniform_id_t id, uint val1, uint val2, uint val3, uint val4);
-
-        static void set_uniform(uniform_id_t id, float val1);
-        static void set_uniform(uniform_id_t id, float val1, float val2);
-        static void set_uniform(uniform_id_t id, float val1, float val2, float val3);
-        static void set_uniform(uniform_id_t id, float val1, float val2, float val3, float val4);
-
-        static void set_uniform(uniform_id_t id, double val1);
-        static void set_uniform(uniform_id_t id, double val1, double val2);
-        static void set_uniform(uniform_id_t id, double val1, double val2, double val3);
-        static void set_uniform(uniform_id_t id, double val1, double val2, double val3, double val4);
-
-        static void set_uniform(uniform_id_t id, const glm::mat4& matrix);
-
-        template <typename T>
-        static void set_uniform(uniform_id_t id, const core::vec<T, 2>& vec) {
-            set_uniform(id, vec.x(), vec.y());
-        }
-
-        template <typename T>
-        static void set_uniform(uniform_id_t id, const core::vec<T, 3>& vec) {
-            set_uniform(id, vec.x(), vec.y(), vec.z());
-        }
-
-        template <typename T>
-        static void set_uniform(uniform_id_t id, const core::vec<T, 4>& vec) {
-            set_uniform(id, vec.x(), vec.y(), vec.z(), vec.w());
-        }
-
-        template <typename T, typename... Ts>
-        void set_uniform(shader_program_id_t id, const core::string& name, T val, Ts... values) {
-            set_uniform(get_uniform_id_unwrap(id, name), val, values...);
-        }
-
-        static void use_program(shader_program_id_t program);
-
-    private:
-        core::hash_map<core::pair<core::string, shader_program_id_t>, uniform_id_t>       _uniforms;
-        core::hash_map<core::pair<shader_effect_id_t, core::string>, shader_program_id_t> _programs;
-        core::hash_map<core::string, shader_effect_id_t>                                  _effects;
-    };
+private:
+    core::hash_map<core::string, core::shared_ptr<grx_shader_program>> _program_map;
+};
 } // namespace grx
