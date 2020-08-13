18a90f3c94becd961d0914a711d5a013531f85eb
Slava Aseev <sockskilling@gmail.com>
2020-08-13 23:51:24 +0300

    Add genshit sample (GPU)

--- src/samples/genshit.cpp
+++ src/samples/genshit.cpp
@@ -1,81 +1,53 @@
 #include <core/main.cpp>
 #include <core/container_extensions.hpp>
 #include <graphics/grx_color_map.hpp>
-#include <random>
+#include <graphics/grx_shader.hpp>
+#include <graphics/grx_window.hpp>
 
 using namespace core;
 using namespace grx;
 
-using block_storage_t = vector<vector<float_color_rgb>>;
-
-block_storage_t wrap_to_blocks(const grx_color_map<float, 3>& img, uint quad_size) {
-    block_storage_t blocks;
-
-    for (auto y : uindex_seq(0, img.size().y(), quad_size)) {
-        for (auto x : uindex_seq(0, img.size().x(), quad_size)) {
-            auto& back = blocks.emplace_back(quad_size*quad_size);
-
-            for (auto h : uindex_seq(y, y + quad_size))
-                for (auto w : uindex_seq(x, x + quad_size))
-                    back.at((h-y)*quad_size + (w - x)) = img[h][w].get();
-        }
-    }
-
-    return blocks;
+auto load_image(const string& path, uint bs) {
+    auto img = load_color_map_unwrap<float_color_rgb>(path);
+    auto wrapped_size = (img.size() / bs) * bs;
+    return img.get_resized(wrapped_size);
 }
 
 int pe_main(args_view args) {
-    auto quad_size = args.by_key_default<uint>({"--quad", "-q"}, 16);
-    auto path1     = args.next_unwrap("Missing path to target image");
-    auto path2     = args.next_unwrap("Missing path to source image");
+    auto bs    = args.by_key_default<uint>({"--quad", "-q"}, 16);
+    auto path1 = args.next_unwrap("Missing path to target image");
+    auto path2 = args.next_unwrap("Missing path to source image");
+    args.require_end();
 
-    auto img = load_color_map_unwrap<float_color_rgb>(path1);
-    auto wrapped_size = img.size();
-    wrapped_size.x() = (wrapped_size.x() / quad_size) * quad_size;
-    wrapped_size.y() = (wrapped_size.y() / quad_size) * quad_size;
+    auto img1 = load_image(path1, bs);
+    auto img2 = load_image(path2, bs);
 
-    img       = img.get_resized(wrapped_size);
-    auto img2 = load_color_map_unwrap<float_color_rgb>(path2).get_resized(wrapped_size);
+    for (auto src_pos : dimensional_seq(img1.size() / bs)) {
+        auto src_start = src_pos * bs;
+        auto best_cnv  = numlim<float>::max();
+        auto best_pos  = vec{0U, 0U};
 
-    auto blocks     = wrap_to_blocks(img, quad_size);
-    auto blocks2    = wrap_to_blocks(img2, quad_size);
-    auto new_blocks = blocks;
-
-    auto compare = [](auto& l, auto& r) {
-        float pts = 0;
-        for (auto& [a, b] : zip_view(l, r)) {
-            auto v = a - b;
-            pts += abs(v.x()) + abs(v.y()) + abs(v.z());
-        }
-        return pts;
-    };
-
-    for (size_t block_idx : index_view(blocks)) {
-        print("\r{}/{}", block_idx, blocks.size());
-        std::cout << std::flush;
-
-        size_t found = 0;
-        float points = std::numeric_limits<float>::max();
-        for (size_t i : index_view(blocks2)) {
-            float v = compare(blocks[block_idx], blocks2[i]);
-            if (v < points) {
-                found = i;
-                points = v;
+        printfl("\r{}/{}", src_pos, img1.size() / bs);
+        for (auto smp_pos : dimensional_seq(img2.size() / bs)) {
+            float cnv      = 0.f;
+            auto smp_start = smp_pos * bs;
+            for (auto [x, y] : dimensional_seq(vec{bs, bs})) {
+                auto result = abs(img2[smp_start.y() + y][smp_start.x() + x].get() -
+                        img1[src_start.y() + y][src_start.x() + x].get());
+                cnv += result.x() + result.y() + result.z();
+            }
+            if (cnv < best_cnv) {
+                best_cnv = cnv;
+                best_pos = smp_start;
             }
         }
-
-        new_blocks[block_idx] = blocks2[found];
+        for (auto [x, y] : dimensional_seq(vec{bs, bs}))
+            img1[src_start.y() + y][src_start.x() + x] =
+                img2[best_pos.y() + x][best_pos.x() + x].get();
     }
 
-    for (size_t i : index_view(new_blocks)) {
-        size_t x_start = (i * quad_size) % img.size().x();
-        size_t y_start = ((i * quad_size) / img.size().x()) * quad_size;
-
-        for (size_t k : index_seq(quad_size*quad_size))
-            img[y_start + k / quad_size][x_start + k % quad_size] = new_blocks[i].at(k);
-    }
-
-    save_color_map_unwrap(img, path1 + ".edit.jpg");
+    save_color_map_unwrap(img1, path1 + ".edit.jpg");
 
     return 0;
 }
+
---
+++ src/samples/genshit_gpu.cpp
@@ -0,0 +1,83 @@
+#include <core/main.cpp>
+#include <core/container_extensions.hpp>
+#include <graphics/grx_color_map.hpp>
+#include <graphics/grx_ssbo.hpp>
+#include <graphics/grx_shader.hpp>
+#include <graphics/grx_texture.hpp>
+#include <graphics/grx_window.hpp>
+
+using namespace core;
+using namespace grx;
+
+auto load_texture(const string& path, uint bs) {
+    auto img = load_color_map_unwrap<float_color_rgba>(path);
+    auto wrapped_size = (img.size() / bs) * bs;
+    return grx_texture(img.get_resized(wrapped_size));
+}
+
+int pe_main(args_view args) {
+    auto fake_wnd = grx_window("fake", {100, 100});
+    fake_wnd.make_current();
+
+    auto bs    = args.by_key_default<uint>({"--quad", "-q"}, 16);
+    auto path1 = args.next_unwrap("Missing path to target image");
+    auto path2 = args.next_unwrap("Missing path to source image");
+    args.require_end();
+
+    auto txt1  = load_texture(path1, bs);
+    auto txt2  = load_texture(path2, bs);
+
+    auto shader = grx_shader_program::create_shared(grx_shader<shader_type::compute>{
+        "layout(local_size_variable) in;"
+        "uniform int bs;"
+        "uniform ivec2 src_size;"
+        "uniform ivec2 smp_size;"
+        "layout(rgba8, binding = 0) uniform restrict image2D src_img;"
+        "layout(rgba8, binding = 1) uniform readonly image2D smp_img;"
+        "void main() {"
+        "    int   id_x      = int(gl_GlobalInvocationID.x);"
+        "    ivec2 src_bs    = src_size / bs;"
+        "    ivec2 smp_bs    = smp_size / bs;"
+        "    ivec2 src_start = ivec2(mod(id_x, src_bs.x), id_x / src_bs.x) * bs;"
+        "    ivec2 best_pos  = ivec2(0, 0);"
+        "    float best_cnv  = 99999999.0;"
+        "    for (int by = 0; by < smp_bs.y; ++by) {"
+        "        for (int bx = 0; bx < smp_bs.x; ++bx) {"
+        "            float cnv       = 0.0;"
+        "            ivec2 smp_start = ivec2(bx, by) * bs;"
+        "            for (int y = 0; y < bs; ++y) {"
+        "                for (int x = 0; x < bs; ++x) {"
+        "                    vec4 src_color = imageLoad(src_img, src_start + ivec2(x, y));"
+        "                    vec4 smp_color = imageLoad(smp_img, smp_start + ivec2(x, y));"
+        "                    vec4 result    = abs(smp_color - src_color);"
+        "                    cnv += result.x + result.y + result.z;"
+        "                }"
+        "            }"
+        "            if (cnv < best_cnv) {"
+        "                best_cnv = cnv;"
+        "                best_pos = smp_start;"
+        "            }"
+        "        }"
+        "    }"
+        "    for (int y = 0; y < bs; ++y) {"
+        "        for (int x = 0; x < bs; ++x) {"
+        "            vec4 color = imageLoad(smp_img, best_pos + ivec2(x, y));"
+        "            imageStore(src_img, src_start + ivec2(x, y), color);"
+        "        }"
+        "    }"
+        "}"
+    });
+
+    shader->get_uniform_unwrap<int>  ("bs")       = static_cast<int>(bs);
+    shader->get_uniform_unwrap<vec2i>("src_size") = static_cast<vec2i>(txt1.size());
+    shader->get_uniform_unwrap<vec2i>("smp_size") = static_cast<vec2i>(txt2.size());
+
+    shader->activate();
+    txt1.bind_level<0>(0, texture_access::readwrite);
+    txt2.bind_level<1>(0);
+    shader->dispatch_compute(txt1.size().x() / bs, 1, 1, txt1.size().y() / bs, 1, 1);
+    save_color_map_unwrap(txt1.to_color_map(), path1 + ".edit.jpg");
+
+    return 0;
+}
+
