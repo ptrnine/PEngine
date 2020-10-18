0795edeb2d184752880d84fb9f8b69a89d2ab2da
Slava Aseev <sockskilling@gmail.com>
2020-10-19 02:15:05 +0300

    Implement scope guard

---
+++ src/core/scope_guard.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <functional>
+
+namespace core
+{
+template <typename F, typename FF = decltype(std::function{std::declval<F>()})>
+class scope_guard {
+public:
+    scope_guard(F&& function): func(std::forward<F>(function)) {}
+    ~scope_guard() noexcept(noexcept(std::declval<F>()())) {
+        if (func) {
+            if constexpr (noexcept(std::declval<F>()()))
+                func();
+            else {
+                if (std::uncaught_exceptions()) {
+                    try {
+                        func();
+                    }
+                    catch (...) {
+                        std::terminate();
+                    }
+                }
+                else {
+                    func();
+                }
+            }
+        }
+    }
+    scope_guard(scope_guard&&) = delete;
+    scope_guard& operator=(scope_guard&&) = delete;
+    scope_guard(const scope_guard&)       = delete;
+    scope_guard& operator=(const scope_guard&) = delete;
+
+    void dismiss() {
+        func = {};
+    }
+
+private:
+    FF func;
+};
+
+} // namespace core
