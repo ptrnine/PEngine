af99973801601b557a94832f69290a68335f4ea9
Slava Aseev <sockskilling@gmail.com>
2021-01-01 08:38:02 +0300

    CMake/git_submodule_update: increase timeout

--- CMake/git_submodule_update.cmake
+++ CMake/git_submodule_update.cmake
@@ -3,7 +3,7 @@ macro(git_submodule_update)
     if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
         message(STATUS "Submodule update")
         execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-                TIMEOUT 120
+                TIMEOUT 100000
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                 RESULT_VARIABLE GIT_SUBMOD_RESULT)
         if(NOT GIT_SUBMOD_RESULT EQUAL "0")
