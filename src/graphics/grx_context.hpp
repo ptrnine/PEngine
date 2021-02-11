08fdc0732775c139c415d792824ee18fefd565ea
Slava Aseev <sockskilling@gmail.com>
2021-02-12 01:05:09 +0300

    grx_context: add counter for drawable vertices

--- src/graphics/grx_context.hpp
+++ src/graphics/grx_context.hpp
@@ -16,7 +16,14 @@ namespace grx {
         void set_depth_mask_enabled(bool value);
         void set_cull_face_enabled(bool value);
 
-        //void enable_be
+
+        void append_drawed_vertices(size_t count) {
+            _drawed_vertices += count;
+        }
+
+        void update_states() {
+            _drawed_vertices = 0;
+        }
 
     private:
         grx_context();
@@ -28,10 +35,14 @@ namespace grx {
         bool _is_cull_face_enabled  = true;
         bool _is_depth_mask_enabled = true;
 
+
+        size_t _drawed_vertices = 0;
+
     public:
         DECLARE_VAL_GET(is_wireframe_enabled)
         DECLARE_VAL_GET(is_depth_test_enabled)
         DECLARE_VAL_GET(is_cull_face_enabled)
+        DECLARE_VAL_GET(drawed_vertices)
     };
 
     inline grx_context& grx_ctx() {
