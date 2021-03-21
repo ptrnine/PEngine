255f1d81e242d9ddaf5bc6701a949503ed2e6034
Slava Aseev <sockskilling@gmail.com>
2021-03-21 21:42:19 +0300

    core/helper_macros: add to_tuple macro for structs

--- src/core/helper_macros.hpp
+++ src/core/helper_macros.hpp
@@ -58,6 +58,14 @@ auto to_tuple() const { \
     return std::make_tuple(__VA_ARGS__); \
 }
 
+#define TUPLE_SERIALIZE(...) \
+auto to_tuple() const { \
+    return std::make_tuple(__VA_ARGS__); \
+} \
+void set_from_tuple(decltype(std::make_tuple(__VA_ARGS__)) tuple_data) { \
+    std::tie(__VA_ARGS__) = std::move(tuple_data); \
+}
+
 #define DEBUG_VAL_LMAO(value) \
 std::cout << #value ": " << (value) << std::endl
 
@@ -73,3 +81,4 @@ std::cout << #value ": " << (value) << std::endl
 #define xlambda2(ARGUMENT_NAME1, ARGUMENT_NAME2, ...) \
     ([](const auto& ARGUMENT_NAME1, const auto& ARGUMENT_NAME2) { return __VA_ARGS__; })
 
+#include "macro_argname.hpp"
